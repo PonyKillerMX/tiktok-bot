71f4e89b4d1aff18b0c39a098a00043c568116d3
PonykillerMX <emilioanderevalencia@gmail.com>
2021-02-05 10:37:19 -0500

    editing my own file

--- emi.py
+++ emi.py
@@ -9,7 +9,11 @@ async def main():
     browser = await launch()
     page = await browser.newPage() 
     tiktok = await page.goto("https://www.tiktok.com")
+    
+    # Click on login button 
+    click_upload_video = await page.click("#main > div.jsx-1217828260.header-container.white.middle > div > div.jsx-2365341634.menu-right > div > div > span > svg > path") 
 
+    sleep(5) 
     screen_shot = await page.screenshot(screenshot)
     print("Took ss") 
     await browser.close() 
