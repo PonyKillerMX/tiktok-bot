6724c71b077f3ad31dc64845950b7070301ad62d
PonykillerMX <emilioanderevalencia@gmail.com>
2021-02-08 14:43:35 -0500

    added some print statements

--- emi.py
+++ emi.py
@@ -9,13 +9,15 @@ async def main():
     browser = await launch()
     page = await browser.newPage() 
     tiktok = await page.goto(URL)
-    
+    print("Opened tiktok")    
     # Click on login button 
     click_upload_video = await page.click(upload_video) 
     # Click on the login with username button
+    print("Clicked on upload video") 
     sleep(5) 
     click_user_await = await page.waitForSelector(login_username)
     click_user = await page.click(login_username) 
+    print("Clicked on username login") 
     screen_shot = await page.screenshot(screenshot)
     print("Took ss") 
     await browser.close() 
--- paths.py
+++ paths.py
@@ -21,7 +21,7 @@ pass_selector = 'input[name="password"]'
 
 admin_role_tag = "#popout_477 > div > div > div.body-3iLsc4.thin-1ybCId.scrollerBase-289Jih.fade-2kXiP2 > div.bodyInnerWrapper-Z8WDxe > div.root-3-B5F3.flex-1O1GKY.wrap-ZIn9Iy.rolesList-22qj2L.endBodySection-Rf4s-7 > div > div.roleName-32vpEy"
 
-login_username = "#root > div > div.tiktok-web-body-33PDi.tiktok-web-body-modal-2N5Wt > div > div.login-container-4HZX-.modal-container-fsYki > div.social-container-NE2xk > div:nth-child(2)"
+login_username = "#root > div > div.tiktok-web-body-33PDi.tiktok-web-body-modal-2N5Wt > div > div.login-container-4HZX-.modal-container-fsYki > div.social-container-NE2xk > div:nth-child(1) > div.channel-name-2qzLW"
 
 upload_video = "#main > div.jsx-1217828260.header-container.white.middle > div > div.jsx-2365341634.menu-right > div > div > span > svg > path"
 
